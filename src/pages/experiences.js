df5ace588b8ce145990933b0110dda940a7faccc
Amy Salley <salleya@oregonstate.edu>
2022-10-27 16:35:46 -0600

    experiences page

--- src/pages/experiences.js
+++ src/pages/experiences.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../css/experiences.css'
 import ExperiencesTable from '../components/Experiences/ExperiencesTable';
-import { searchResults } from '../components/Experiences/ExperiencesResults';
+import { searchResults } from '../services/experiences';
 
 
 const Experiences = () => {
+
 
     return (
         <div class="exp-page">
