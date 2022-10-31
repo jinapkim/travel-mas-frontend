5bb0b1d65b8c0cb582416830b2bf0339ee3a7b22
Amy Salley <salleya@oregonstate.edu>
2022-10-31 09:48:14 -0600

    show all experiences

--- src/pages/experiences.js
+++ src/pages/experiences.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import '../css/experiences.css'
+import { useState, useEffect } from 'react';
 import ExperiencesTable from '../components/Experiences/ExperiencesTable';
-import { searchResults } from '../services/experiences';
 
 
-const Experiences = () => {
+function Experiences() {
+    
+    //const data = require('../components/Experiences/experiences-sample-data.json');
+    //const searchResults = data.experiences;
 
+    const [searchResults, setSearchResults] = useState([]);
+
+    const loadExperiences = async () => {
+        const response = await fetch('http://127.0.0.1:5000/experiences');
+        const data = await response.json();
+        setSearchResults(data.experiences);
+    }
+
+    useEffect(() => {
+        loadExperiences();
+    }, []);
 
     return (
         <div class="exp-page">
             <h1>Experiences</h1>
-            <ExperiencesTable results={searchResults}></ExperiencesTable>
+            <ExperiencesTable results={ searchResults }></ExperiencesTable>
         </div>
     );  
 }
