0e2fadb20acb542e7aa0a3897c52c026e73567d4
Amy Salley <salleya@oregonstate.edu>
2022-11-03 15:45:07 -0600

    display all experiences

--- src/components/Experiences/ExperiencesRow.js
+++ src/components/Experiences/ExperiencesRow.js
@@ -3,23 +3,22 @@ import { AiFillLike, AiOutlineLike, AiOutlineEdit, AiOutlineDelete } from "react
 function ExperiencesRow({ result }) {
     
     return (
-        
-        <div class="box">
-            <div class="shadow-box">
-                <img class="card-img" src={result.image_url} alt={result.title+' image'} ></img>
-                <div class="container">
+        <div className="box">
+            <div className="shadow-box">
+                <img className="card-img" src={result.image_url} alt={result.title+' image'} ></img>
+                <div className="container">
                     <h3>{result.title}</h3>
                     <li>Description: {result.description}</li>
                     <li>Location: {result.location}</li>
                     <li>Coordinates: {result.geo_location}</li>
                     <li>Rating: {result.rating}</li>
                     <br></br>
-                    <a href="/"><AiOutlineDelete class='icon'/></a>
-                    <a href="/"><AiOutlineEdit class='icon'/></a>
-                    <a href="/"><AiOutlineLike class='icon'/></a>
+                    <a href="/"><AiOutlineDelete className='icon'/></a>
+                    <a href="/"><AiOutlineEdit className='icon'/></a>
+                    <a href="/"><AiOutlineLike className='icon'/></a>
                     <br></br>
                 </div>
-            </div> 
+            </div>
         </div>
     )
 }
