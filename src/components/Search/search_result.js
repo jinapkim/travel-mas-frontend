2b25b67bb2c88677f9c6464ef073da9db4719e19
jinapkim <jinakimchi@gmail.com>
2022-11-21 13:12:34 -0500

    updated search bar/search results

--- src/components/Search/search_result.js
+++ src/components/Search/search_result.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import ExperiencesTable from '../../components/Experiences/ExperiencesTable';
 
@@ -7,8 +7,16 @@ function SearchResult() {
     const navigate = useNavigate();
     const location = useLocation();
 
+    const [experienceToView, setExperienceToView] = useState([]);
     const [experienceToEdit, setExperienceToEdit] = useState([]);
     const [searchResults, setSearchResults] = useState([]);
+    const [userTrips, setUserTrips] = useState([
+        {
+          name: "No Trips Found",
+          id: -1,
+        },
+      ]);
+    const [userLikes, setUserLikes] = useState([]);
 
     const onEdit = async exp_id => {
         const response = await fetch(`${process.env.REACT_APP_API_URL}/${exp_id}`, { 
@@ -39,16 +47,69 @@ function SearchResult() {
         }
     };
 
+    const onView = async (exp_id) => {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/experiences/${exp_id}`
+        );
+        const data = await response.json();
+        setExperienceToView(data);
+        navigate("/view-experience");
+    };
+    
+
     const loadAllExperiences = async () => {
         const response = await fetch(`${process.env.REACT_APP_API_URL}/experiences`);
         const data = await response.json();
         setSearchResults(data.experiences);
     }
+
+    const loadUserTrips = async () => {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/trips?user_id=${localStorage.getItem(
+            "user_id"
+          )}`
+        );
+        const data = await response.json();
+        if (!data.trips.length) {
+          setUserTrips([{
+            name: "No Trips Found",
+            id: -1,
+          }]);
+        } else {
+          setUserTrips(data.trips);
+        }
+    };
+
+    useEffect(() => {
+        loadUserTrips();
+    }, userTrips);
+
+    useEffect(() => {
+    loadAllExperiences();
+    }, []);
+
+    const loadUserLikes = async () => {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/ratings`);
+        const data = await response.json();
+        setUserLikes(data.ratings);
+    };
+
+    useEffect(() => {
+    loadUserLikes();
+    }, []);
     
     return (
         <>
             <h1>Results for "{location.state.query}"</h1>
-            <ExperiencesTable results={location.state.experiences} onDelete={onDelete} onEdit={onEdit}></ExperiencesTable>
+            <ExperiencesTable 
+                results={location.state.experiences} 
+                onDelete={onDelete} 
+                onEdit={onEdit}
+                onView={onView}
+                likes={userLikes}
+                trips={userTrips}
+            >
+            </ExperiencesTable>
         </>
     );
 }
